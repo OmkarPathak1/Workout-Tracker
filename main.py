23b80776d344e9a628bb158deea83b357a550778
Omkar Pathak <92076273+OmkarPathak1@users.noreply.github.com>
2023-05-03 13:34:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+from datetime import datetime
+
+GENDER = 'GENDER'
+WEIGHT_KG = 'WEIGHT'
+HEIGHT_CM = 'HEIGHT'
+AGE = 'AGE'
+
+APP_ID = 'API ID'
+API_KEY = 'API KEY'
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "SHEETY API"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # Basic Authentication
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        auth=(
+                'USERNAME',
+                'PASSWORD',
+        )
+    )
+
